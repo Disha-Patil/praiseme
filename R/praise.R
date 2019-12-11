89ce2da85b708b452fa913093f1e7c8d4caada40
Disha-Patil <dishapatil10@gmail.com>
2019-12-11 10:03:23 +0530

    description typos

--- R/praise.R
+++ R/praise.R
@@ -3,7 +3,7 @@
 #' @description This function is super useful when you are feeling sad. It works really well.
 #'
 #' @param name This is the name of then person I want to praise. Input as text string.
-#' @param punctuation This is our emphasis as a text input.
+#' @param punctuation This is our emphasis as a text input and default is exclamation mark.
 #'
 #' @return Text string with praise
 #' @export
