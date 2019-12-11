5d5034f1fbedd415f3b09e6be0311046788d14ef
Disha-Patil <dishapatil10@gmail.com>
2019-12-11 10:17:06 +0530

    check credential cache

--- R/praise.R
+++ R/praise.R
@@ -3,7 +3,7 @@
 #' @description This function is super useful when you are feeling sad. It works really well.
 #'
 #' @param name This is the name of then person I want to praise. Input as text string.
-#' @param punctuation This is our emphasis as a text input and default is exclamation mark.
+#' @param punctuation This is our emphasis as a text input and default is exclamation mark and lets keep it that way.
 #'
 #' @return Text string with praise
 #' @export
