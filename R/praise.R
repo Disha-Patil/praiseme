d0ec30840a6c00ebb00a3895ccec77ad1c49db94
Disha-Patil <dishapatil10@gmail.com>
2019-12-11 10:01:25 +0530

    A commit to add default punctuation and updated documentation.

--- R/praise.R
+++ R/praise.R
@@ -1,6 +1,6 @@
 #' Deliver praise
 #'
-#' @description This function is super useful when you are feeling sad.
+#' @description This function is super useful when you are feeling sad. It works really well.
 #'
 #' @param name This is the name of then person I want to praise. Input as text string.
 #' @param punctuation This is our emphasis as a text input.
@@ -10,7 +10,7 @@
 #'
 #' @examples
 #' praise("disha",".")
-praise <- function(name, punctuation) {
+praise <- function(name, punctuation="!") {
   paste0("You are the best, ", name, punctuation)
 }
 
